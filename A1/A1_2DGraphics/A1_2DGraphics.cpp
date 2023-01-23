7c660afefc71a222d8146df2fba4d526b0f399b4
Saif Mahmud <60409889+vmsaif@users.noreply.github.com>
2023-01-22 22:58:47 -0600

    starting A1

---
+++ A1/A1_2DGraphics/A1_2DGraphics.cpp
@@ -0,0 +1,13 @@
+// A1_2DGraphics.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+#include "glut.h"
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
